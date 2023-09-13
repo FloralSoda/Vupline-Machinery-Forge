e51eec36a23221eaad2b9f6ab92064cefb9bda93
Floral Soda <lilysoda@pm.me>
2023-09-13 22:04:13 +0100

    Updated assets to new versions.

--- src/main/java/xyz/hydroxa/vulpine_machinery/item/ModItems.java
+++ src/main/java/xyz/hydroxa/vulpine_machinery/item/ModItems.java
@@ -1,4 +1,21 @@
 package xyz.hydroxa.vulpine_machinery.item;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import xyz.hydroxa.vulpine_machinery.VulpineMachineryMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, VulpineMachineryMod.MOD_ID);
+
+    public static final RegistryObject<Item> BULLET = ITEMS.register("bullet",
+            () -> new Item(new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_COMBAT)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
